18efe9e0a7013ff98667d9bd52814cb181aa1d31
정지원 <jiwon936626@naver.com>
2024-05-09 01:06:38 +0900

    풍향 추가

--- src/screen/weatherDetail/WindDirectionScreen.tsx
+++ src/screen/weatherDetail/WindDirectionScreen.tsx
@@ -1,5 +1,34 @@
-import { View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
+import { isTablet } from "store"
+import { CommonStyle, MobileFont, TabletFont } from "style/CommonStyle"
+import MobileCompass from "asset/image/image_mobile_compass.svg"
+import TabletCompass from "asset/image/image_tablet_compass.svg"
+import { WeatherDetailFooter } from "component/CommonComponent"
 
-export const WindDirectionScreen = () => {
-    return <View style={{ flex: 1, backgroundColor: "#fff" }} />
+export const WindDirectionScreen = ({ footerText }: { footerText: string }) => {
+    return (
+        <View>
+            <Text style={[styles.title, isTablet ? TabletFont.heading_2 : MobileFont.body_1]}>풍향이란?</Text>
+            <Text style={[styles.content, isTablet ? TabletFont.weather_info_main_text : MobileFont.weather_info_main_text]}>풍향은 바람이 불어오는 방향을 의미하며 오늘모입지에서는 8방위각도를 활용해 풍향을 나타냅니다.</Text>
+            <View style={styles.title}>
+                <Text style={[isTablet ? TabletFont.heading_2 : MobileFont.body_1]}>풍향 구분</Text>
+                <View style={[CommonStyle.center, styles.compass]}>{isTablet ? <TabletCompass /> : <MobileCompass />}</View>
+            </View>
+            {!isTablet && <WeatherDetailFooter text={footerText} />}
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    compass: {
+        marginTop: isTablet ? 32 : 16,
+        marginBottom: 52
+    },
+    content: {
+        marginTop: 8
+    },
+    title: {
+        marginTop: 32,
+        justifyContent: "space-between"
+    }
+})
